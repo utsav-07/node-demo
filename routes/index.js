c8060c491db8d434bab88efddb9c68dd5b81d8e9
utsav <utsavudit07@gmail.com>
2020-08-30 12:17:07 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
+    message: "express js starter project",
+    welcome: "swagatam To",
+  });
+});
+
+router.get("/getimage", function (req, res, next) {
+  res.render("viewimage", {
+    title: "Gallery",
+    msg: "Welcome To My Gallery",
+  });
+});
+
+module.exports = router;
